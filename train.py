cf1d313b7df6176044c33b3f656307ee0babbd77
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-05-01 04:53:56 +0000

    code_1 copied from existing folder

---
+++ train.py
@@ -0,0 +1,373 @@
+import pandas as pd, numpy as np, gc
+# from kaggle_datasets import KaggleDatasets
+import tensorflow as tf, re, math
+import tensorflow.keras.backend as K
+import efficientnet.tfkeras as efn
+# from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+# import matplotlib.pyplot as plt
+
+"""**Model parameters**"""
+
+DEVICE='TPU'
+
+SEED=np.random.randint(1,100)
+
+IMG_SIZES=[224,224]
+
+# COARSE DROPOUT
+DROP_FREQ = [0,0.75,0.75] # between 0 and 1
+DROP_CT = [0,8,8] # may slow training if CT>16
+DROP_SIZE = [0,0.2,0.2] # between 0 and 1
+
+BATCH_SIZES = [32]
+EPOCHS = [10]
+
+"""**TPU configuration** 
+
+
+"""
+
+if DEVICE == "TPU":
+    print("connecting to TPU...")
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='tpu')
+        print('Running on TPU ', tpu.master())
+    except ValueError:
+        print("Could not connect to TPU")
+        tpu = None
+
+    if tpu:
+        try:
+            print("initializing  TPU ...")
+            tf.config.experimental_connect_to_cluster(tpu)
+            tf.tpu.experimental.initialize_tpu_system(tpu)
+            strategy = tf.distribute.TPUStrategy(tpu)
+            print("TPU initialized")
+        except:
+            print("failed to initialize TPU")
+    else:
+        DEVICE = "GPU"
+
+if DEVICE != "TPU":
+    print("Using default strategy for CPU and single GPU")
+    strategy = tf.distribute.get_strategy()
+
+if DEVICE == "GPU":
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+    
+
+AUTO     = tf.data.experimental.AUTOTUNE
+REPLICAS = strategy.num_replicas_in_sync
+print(f'REPLICAS: {REPLICAS}')
+
+# 'gs://kds-7519c027b63db7c377559316c0d930960fd61347d4086351a37c897f'
+
+GCS_PATH='gs://project-285401/512_tfrec'
+IDX=[2,3,4,5]
+files_train = list(tf.io.gfile.glob(GCS_PATH + '/train*.tfrec'))
+files_test  = np.sort(np.array(tf.io.gfile.glob(GCS_PATH + '/test*.tfrec')))
+from sklearn.model_selection import train_test_split
+files_train,files_valid=train_test_split(files_train,random_state=SEED,test_size=0.2)
+GCS_PATH2    = 'gs://project-285401/malignant_512_tfrec'
+MALIGNANT = [GCS_PATH2 + '/train*.tfrec']
+files_train+= list(np.asarray(tf.io.gfile.glob(MALIGNANT)))
+files_train=np.array(files_train)
+files_valid=np.array(files_valid)
+np.random.shuffle(files_train)
+
+"""**Data augumentation**"""
+
+ROT_ = 180.0; SHR_ = 2.0
+HZOOM_ = 8.0; WZOOM_ = 8.0
+HSHIFT_ = 8.0; WSHIFT_ = 8.0
+
+def get_mat(rotation, shear, height_zoom, width_zoom, height_shift, width_shift):
+    # returns 3x3 transformmatrix which transforms indicies
+        
+    # CONVERT DEGREES TO RADIANS
+    rotation = math.pi * rotation / 180.
+    shear    = math.pi * shear    / 180.
+
+    def get_3x3_mat(lst):
+        return tf.reshape(tf.concat([lst],axis=0), [3,3])
+    
+    # ROTATION MATRIX
+    c1   = tf.math.cos(rotation)
+    s1   = tf.math.sin(rotation)
+    one  = tf.constant([1],dtype='float32')
+    zero = tf.constant([0],dtype='float32')
+    
+    rotation_matrix = get_3x3_mat([c1,   s1,   zero, 
+                                   -s1,  c1,   zero, 
+                                   zero, zero, one])    
+    # SHEAR MATRIX
+    c2 = tf.math.cos(shear)
+    s2 = tf.math.sin(shear)    
+    
+    shear_matrix = get_3x3_mat([one,  s2,   zero, 
+                                zero, c2,   zero, 
+                                zero, zero, one])        
+    # ZOOM MATRIX
+    zoom_matrix = get_3x3_mat([one/height_zoom, zero,           zero, 
+                               zero,            one/width_zoom, zero, 
+                               zero,            zero,           one])    
+    # SHIFT MATRIX
+    shift_matrix = get_3x3_mat([one,  zero, height_shift, 
+                                zero, one,  width_shift, 
+                                zero, zero, one])
+    
+    return K.dot(K.dot(rotation_matrix, shear_matrix), 
+                 K.dot(zoom_matrix,     shift_matrix))
+
+
+def transform(image, DIM=256):    
+    # input image - is one image of size [dim,dim,3] not a batch of [b,dim,dim,3]
+    # output - image randomly rotated, sheared, zoomed, and shifted
+    XDIM = DIM%2 #fix for size 331
+    
+    rot = ROT_ * tf.random.normal([1], dtype='float32')
+    shr = SHR_ * tf.random.normal([1], dtype='float32') 
+    h_zoom = 1.0 + tf.random.normal([1], dtype='float32') / HZOOM_
+    w_zoom = 1.0 + tf.random.normal([1], dtype='float32') / WZOOM_
+    h_shift = HSHIFT_ * tf.random.normal([1], dtype='float32') 
+    w_shift = WSHIFT_ * tf.random.normal([1], dtype='float32') 
+
+    # GET TRANSFORMATION MATRIX
+    m = get_mat(rot,shr,h_zoom,w_zoom,h_shift,w_shift) 
+
+    # LIST DESTINATION PIXEL INDICES
+    x   = tf.repeat(tf.range(DIM//2, -DIM//2,-1), DIM)
+    y   = tf.tile(tf.range(-DIM//2, DIM//2), [DIM])
+    z   = tf.ones([DIM*DIM], dtype='int32')
+    idx = tf.stack( [x,y,z] )
+    
+    # ROTATE DESTINATION PIXELS ONTO ORIGIN PIXELS
+    idx2 = K.dot(m, tf.cast(idx, dtype='float32'))
+    idx2 = K.cast(idx2, dtype='int32')
+    idx2 = K.clip(idx2, -DIM//2+XDIM+1, DIM//2)
+    
+    # FIND ORIGIN PIXEL VALUES           
+    idx3 = tf.stack([DIM//2-idx2[0,], DIM//2-1+idx2[1,]])
+    d    = tf.gather_nd(image, tf.transpose(idx3))
+        
+    return tf.reshape(d,[DIM, DIM,3])
+
+def dropout(image, DIM=256, PROBABILITY = 0.75, CT = 8, SZ = 0.2):
+    # input image - is one image of size [dim,dim,3] not a batch of [b,dim,dim,3]
+    # output - image with CT squares of side size SZ*DIM removed
+    
+    # DO DROPOUT WITH PROBABILITY DEFINED ABOVE
+    P = tf.cast( tf.random.uniform([],0,1)<PROBABILITY, tf.int32)
+    if (P==0)|(CT==0)|(SZ==0): return image
+    
+    for k in range(CT):
+        # CHOOSE RANDOM LOCATION
+        x = tf.cast( tf.random.uniform([],0,DIM),tf.int32)
+        y = tf.cast( tf.random.uniform([],0,DIM),tf.int32)
+        # COMPUTE SQUARE 
+        WIDTH = tf.cast( SZ*DIM,tf.int32) * P
+        ya = tf.math.maximum(0,y-WIDTH//2)
+        yb = tf.math.minimum(DIM,y+WIDTH//2)
+        xa = tf.math.maximum(0,x-WIDTH//2)
+        xb = tf.math.minimum(DIM,x+WIDTH//2)
+        # DROPOUT IMAGE
+        one = image[ya:yb,0:xa,:]
+        two = tf.zeros([yb-ya,xb-xa,3]) 
+        three = image[ya:yb,xb:DIM,:]
+        middle = tf.concat([one,two,three],axis=1)
+        image = tf.concat([image[0:ya,:,:],middle,image[yb:DIM,:,:]],axis=0)
+            
+    # RESHAPE HACK SO TPU COMPILER KNOWS SHAPE OF OUTPUT TENSOR 
+    image = tf.reshape(image,[DIM,DIM,3])
+    return image
+
+def read_labeled_tfrecord(example):
+    tfrec_format = {
+        'image'                        : tf.io.FixedLenFeature([], tf.string),
+        'image_name'                   : tf.io.FixedLenFeature([], tf.string),
+        'target'                       : tf.io.FixedLenFeature([], tf.int64)
+    }           
+    example = tf.io.parse_single_example(example, tfrec_format)
+    return example['image'], example['target']
+
+
+def read_unlabeled_tfrecord(example, return_image_name=True):
+    tfrec_format = {
+        'image'                        : tf.io.FixedLenFeature([], tf.string),
+        'image_name'                   : tf.io.FixedLenFeature([], tf.string),
+    }
+    example = tf.io.parse_single_example(example, tfrec_format)
+    return example['image'], example['image_name'] if return_image_name else 0
+
+ 
+def prepare_image(img, augment=True, dim=256, droprate=0, dropct=0, dropsize=0):    
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.cast(img, tf.float32) / 255.0
+    
+    if augment:
+        img = transform(img,DIM=dim)
+        if (droprate!=0)&(dropct!=0)&(dropsize!=0): 
+            img = dropout(img, DIM=dim, PROBABILITY=droprate, CT=dropct, SZ=dropsize)
+        img = tf.image.random_flip_left_right(img)
+        img = tf.image.random_hue(img, 0.01)
+        img = tf.image.random_saturation(img, 0.7, 1.3)
+        img = tf.image.random_contrast(img, 0.8, 1.2)
+        img = tf.image.random_brightness(img, 0.1)
+                      
+    img = tf.reshape(img, [dim,dim, 3])
+            
+    return img
+
+def count_data_items(filenames):
+    n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) 
+         for filename in filenames]
+    return np.sum(n)
+
+def get_dataset(files, augment = False, shuffle = False, repeat = False, 
+                labeled=True, return_image_names=True, batch_size=16, dim=256,
+                droprate=0, dropct=0, dropsize=0):
+    
+    ds = tf.data.TFRecordDataset(files, num_parallel_reads=AUTO)
+    ds = ds.cache()
+    
+    if repeat:
+        ds = ds.repeat()
+    
+    if shuffle: 
+        ds = ds.shuffle(1024*2) #if too large causes OOM in GPU CPU
+        opt = tf.data.Options()
+        opt.experimental_deterministic = False
+        ds = ds.with_options(opt)
+        
+    if labeled: 
+        ds = ds.map(read_labeled_tfrecord, num_parallel_calls=AUTO)
+    else:
+        ds = ds.map(lambda example: read_unlabeled_tfrecord(example, return_image_names), 
+                    num_parallel_calls=AUTO)      
+    
+    ds = ds.map(lambda img, imgname_or_label: (
+                prepare_image(img, augment=augment, dim=dim, 
+                              droprate=droprate, dropct=dropct, dropsize=dropsize), 
+                imgname_or_label), 
+                num_parallel_calls=AUTO)
+    
+    ds = ds.batch(batch_size * REPLICAS)
+    ds = ds.prefetch(AUTO)
+    return ds
+
+# for i,j in ds.take(1):
+#     print(i.shape)
+#     print(j.shape)
+
+# EFNS = [efn.EfficientNetB0, efn.EfficientNetB1, efn.EfficientNetB2, efn.EfficientNetB3, 
+#         efn.EfficientNetB4, efn.EfficientNetB5, efn.EfficientNetB6]
+
+def binary_focal_loss(gamma=2., alpha=.25):
+    """
+    Binary form of focal loss.
+      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
+      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
+    References:
+        https://arxiv.org/pdf/1708.02002.pdf
+    Usage:
+     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+    def binary_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred:  A tensor resulting from a sigmoid
+        :return: Output tensor.
+        """
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+        epsilon = K.epsilon()
+        # clip to prevent NaN's and Inf's
+        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
+        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
+
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
+               -K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+    return binary_focal_loss_fixed
+def build_model(dim=128):
+    inp = tf.keras.layers.Input(shape=(dim,dim,3))
+    base = efn.EfficientNetB7(input_shape=(dim,dim,3),weights='imagenet',pooling='avg',include_top=False)
+    x = base(inp)
+    # x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(128,activation='gelu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    x = tf.keras.layers.Dense(182,activation='gelu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=inp,outputs=x)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.00001)
+    loss = [binary_focal_loss(gamma=2,alpha=0.12)]
+    model.compile(optimizer=opt,loss=loss,metrics=['AUC'])
+    return model
+
+"""**Learning rate scheduler**"""
+
+def get_lr_callback(batch_size=8):
+    lr_start   = 0.0005
+    lr_max     = 0.000125 * REPLICAS * batch_size
+    lr_min     = 0.00001
+    lr_ramp_ep = 5
+    lr_sus_ep  = 0
+    lr_decay   = 0.8
+   
+    def lrfn(epoch):
+        if epoch < lr_ramp_ep:
+            lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
+            
+        elif epoch < lr_ramp_ep + lr_sus_ep:
+            lr = lr_max
+            
+        else:
+            lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+            
+        return lr
+
+    lr_callback = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=False)
+    return lr_callback
+
+LR = 0.00004;WARMUP = 5
+
+def get_cosine_schedule_with_warmup(lr,num_warmup_steps, num_training_steps, num_cycles=0.5):
+    def lrfn(epoch):
+        if epoch < num_warmup_steps:
+            return (float(epoch) / float(max(1, num_warmup_steps))) * lr
+        progress = float(epoch - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))) * lr
+
+    return tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=True)
+
+lr_schedule= get_cosine_schedule_with_warmup(lr=LR,num_warmup_steps=WARMUP,num_training_steps=EPOCHS[0])
+
+
+"""**Model Training**"""
+
+with strategy.scope():
+        model = build_model(dim=IMG_SIZES[0])
+
+model.summary()
+
+es=tf.keras.callbacks.EarlyStopping(monitor='val_auc',patience=5,restore_best_weights=True,mode='max')
+
+
+# tf=tf.keras.callbacks.TensorBoard(log_dir='gs://project-285401/logs')
+
+history = model.fit(
+        get_dataset(files_train, augment=True, shuffle=True, repeat=True,
+                dim=IMG_SIZES[0], batch_size = BATCH_SIZES[0],
+                droprate=DROP_FREQ[1], dropct=DROP_CT[1], dropsize=DROP_SIZE[1]), 
+        epochs=EPOCHS[0], callbacks = [es,lr_schedule], 
+        steps_per_epoch=count_data_items(files_train)/BATCH_SIZES[0]//REPLICAS,
+        validation_data=get_dataset(files_valid,augment=False,shuffle=False,
+                repeat=False,dim=IMG_SIZES[0]),
+        validation_steps=count_data_items(files_valid)/BATCH_SIZES[0]//REPLICAS
+          )
+
+# model.save('train.h5')
