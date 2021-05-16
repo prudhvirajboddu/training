08a4111c7aab424fa592be1429e5cf012f29d9d2
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-05-16 12:54:47 +0530

    For documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Main Training code of the model
+
+**Dependencies TensorFlow==2.4.1  Python==3.7.x**
--- train.py
+++ train.py
@@ -1,33 +1,55 @@
-import pandas as pd, numpy as np, gc
-# from kaggle_datasets import KaggleDatasets
-import tensorflow as tf, re, math
+import random, re, math
+random.seed(a=42)
+import numpy as np
+from sklearn.model_selection import KFold
+
+import tensorflow as tf
 import tensorflow.keras.backend as K
 import efficientnet.tfkeras as efn
-from augment import *
 
-"""**Model parameters**"""
 
-DEVICE='TPU' #GPU
+GCS_PATH    = "gs://project-285401/70k-512rec" #path to training records
 
-SEED=np.random.randint(1,100)
+files_train = np.sort(np.array(tf.io.gfile.glob(GCS_PATH + '/train*.tfrec')))
+files_test  = np.sort(np.array(tf.io.gfile.glob(GCS_PATH + '/test*.tfrec')))
 
-IMG_SIZES=[500,500] #Tried 512,224,384,256
+DEVICE = "TPU" #GPU
 
-# COARSE DROPOUT for Data Augmentation
-DROP_FREQ = [0,0.75,0.75] # between 0 and 1
-DROP_CT = [0,8,8] # may slow training if CT>16
-DROP_SIZE = [0,0.2,0.2] # between 0 and 1
+bs = 64 #Batch Size
 
-BATCH_SIZES = [32] #Hyperparameter
-EPOCHS = [50] #Tried 64,100 random epochs
+CFG = dict(
+    #hyper parameters 
+    batch_size        =  bs,
+    
+    read_size         = 512, 
+    crop_size         = random.randint(501,512), #Random crop of the image
+    net_size          = 500, 
+    
+    LR_START          =   0.00005,
+    LR_MAX            =   0.000020,
+    LR_MIN            =   0.000001,
+    LR_RAMPUP_EPOCHS  =   5,
+    LR_SUSTAIN_EPOCHS =   0,
+    LR_EXP_DECAY      =   0.8,
+    epochs            =   30,
+    
+    rot               = 180.0,
+    shr               =   2.0,
+    hzoom             =   8.0,
+    wzoom             =   8.0,
+    hshift            =   8.0,
+    wshift            =   8.0,
 
-"""**TPU configuration**"""
-#Model Trained on Both TPU and GPU
+    optimizer         = 'adam',
+    label_smooth_fac  =   0.05,
+    
+    tta_steps         =  25    
+)
 
 if DEVICE == "TPU":
     print("connecting to TPU...")
     try:
-        tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='tpu')
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='tpu') #tpu name is 'tpu'
         print('Running on TPU ', tpu.master())
     except ValueError:
         print("Could not connect to TPU")
@@ -38,9 +60,9 @@ if DEVICE == "TPU":
             print("initializing  TPU ...")
             tf.config.experimental_connect_to_cluster(tpu)
             tf.tpu.experimental.initialize_tpu_system(tpu)
-            strategy = tf.distribute.TPUStrategy(tpu)
+            strategy = tf.distribute.experimental.TPUStrategy(tpu)
             print("TPU initialized")
-        except:
+        except _:
             print("failed to initialize TPU")
     else:
         DEVICE = "GPU"
@@ -57,38 +79,96 @@ AUTO     = tf.data.experimental.AUTOTUNE
 REPLICAS = strategy.num_replicas_in_sync
 print(f'REPLICAS: {REPLICAS}')
 
+def get_mat(rotation, shear, height_zoom, width_zoom, height_shift, width_shift):
+    """Data Augmentation method"""
+    # returns 3x3 transformmatrix which transforms indicies
+        
+    # CONVERT DEGREES TO RADIANS
+    rotation = math.pi * rotation / 180.
+    shear    = math.pi * shear    / 180.
 
-GCS_PATH='gs://project-285401/512_tfrec' #TFrecords for Training
-IDX=[2,3,4,5]
-files_train = list(tf.io.gfile.glob(GCS_PATH + '/train*.tfrec'))
-files_test  = np.sort(np.array(tf.io.gfile.glob(GCS_PATH + '/test*.tfrec')))
-from sklearn.model_selection import train_test_split
-files_train,files_valid=train_test_split(files_train,random_state=SEED,test_size=0.2)
-GCS_PATH2    = 'gs://project-285401/malignant_512_tfrec'
-MALIGNANT = [GCS_PATH2 + '/train*.tfrec']
-files_train+= list(np.asarray(tf.io.gfile.glob(MALIGNANT)))
-files_train=np.array(files_train)
-files_valid=np.array(files_valid)
-np.random.shuffle(files_train)
+    def get_3x3_mat(lst):
+        return tf.reshape(tf.concat([lst],axis=0), [3,3])
+    
+    # ROTATION MATRIX
+    c1   = tf.math.cos(rotation)
+    s1   = tf.math.sin(rotation)
+    one  = tf.constant([1],dtype='float32')
+    zero = tf.constant([0],dtype='float32')
+    
+    rotation_matrix = get_3x3_mat([c1,   s1,   zero, 
+                                   -s1,  c1,   zero, 
+                                   zero, zero, one])    
+    # SHEAR MATRIX
+    c2 = tf.math.cos(shear)
+    s2 = tf.math.sin(shear)    
+    
+    shear_matrix = get_3x3_mat([one,  s2,   zero, 
+                                zero, c2,   zero, 
+                                zero, zero, one])        
+    # ZOOM MATRIX
+    zoom_matrix = get_3x3_mat([one/height_zoom, zero,           zero, 
+                               zero,            one/width_zoom, zero, 
+                               zero,            zero,           one])    
+    # SHIFT MATRIX
+    shift_matrix = get_3x3_mat([one,  zero, height_shift, 
+                                zero, one,  width_shift, 
+                                zero, zero, one])
+    
+    return K.dot(K.dot(rotation_matrix, shear_matrix), 
+                 K.dot(zoom_matrix,     shift_matrix))
 
-"""**Data augumentation**"""
 
-ROT_ = 180.0; SHR_ = 2.0
-HZOOM_ = 8.0; WZOOM_ = 8.0
-HSHIFT_ = 8.0; WSHIFT_ = 8.0
+def transform(image, cfg):    
+    # input image - is one image of size [dim,dim,3] not a batch of [b,dim,dim,3]
+    # output - image randomly rotated, sheared, zoomed, and shifted
+    DIM = cfg["read_size"]
+    XDIM = DIM%2 #fix for size 331
+    
+    rot = cfg['rot'] * tf.random.normal([1], dtype='float32')
+    shr = cfg['shr'] * tf.random.normal([1], dtype='float32') 
+    h_zoom = 1.0 + tf.random.normal([1], dtype='float32') / cfg['hzoom']
+    w_zoom = 1.0 + tf.random.normal([1], dtype='float32') / cfg['wzoom']
+    h_shift = cfg['hshift'] * tf.random.normal([1], dtype='float32') 
+    w_shift = cfg['wshift'] * tf.random.normal([1], dtype='float32') 
 
+    # GET TRANSFORMATION MATRIX
+    m = get_mat(rot,shr,h_zoom,w_zoom,h_shift,w_shift) 
+
+    # LIST DESTINATION PIXEL INDICES
+    x   = tf.repeat(tf.range(DIM//2, -DIM//2,-1), DIM)
+    y   = tf.tile(tf.range(-DIM//2, DIM//2), [DIM])
+    z   = tf.ones([DIM*DIM], dtype='int32')
+    idx = tf.stack( [x,y,z] )
+    
+    # ROTATE DESTINATION PIXELS ONTO ORIGIN PIXELS
+    idx2 = K.dot(m, tf.cast(idx, dtype='float32'))
+    idx2 = K.cast(idx2, dtype='int32')
+    idx2 = K.clip(idx2, -DIM//2+XDIM+1, DIM//2)
+    
+    # FIND ORIGIN PIXEL VALUES           
+    idx3 = tf.stack([DIM//2-idx2[0,], DIM//2-1+idx2[1,]])
+    d    = tf.gather_nd(image, tf.transpose(idx3))
+        
+    return tf.reshape(d,[DIM, DIM,3])
 
 def read_labeled_tfrecord(example):
     tfrec_format = {
         'image'                        : tf.io.FixedLenFeature([], tf.string),
-        'image_name'                   : tf.io.FixedLenFeature([], tf.string),
+        """# 'image_name'                   : tf.io.FixedLenFeature([], tf.string),
+        # 'patient_id'                   : tf.io.FixedLenFeature([], tf.int64),
+        # 'sex'                          : tf.io.FixedLenFeature([], tf.int64),
+        # 'age_approx'                   : tf.io.FixedLenFeature([], tf.int64),
+        # 'anatom_site_general_challenge': tf.io.FixedLenFeature([], tf.int64),
+        # 'diagnosis'                    : tf.io.FixedLenFeature([], tf.int64),
+        Model only for images not metadata"""
         'target'                       : tf.io.FixedLenFeature([], tf.int64)
     }           
     example = tf.io.parse_single_example(example, tfrec_format)
     return example['image'], example['target']
 
 
-def read_unlabeled_tfrecord(example, return_image_name=True):
+def read_unlabeled_tfrecord(example, return_image_name):
     tfrec_format = {
         'image'                        : tf.io.FixedLenFeature([], tf.string),
         'image_name'                   : tf.io.FixedLenFeature([], tf.string),
@@ -97,22 +177,25 @@ def read_unlabeled_tfrecord(example, return_image_name=True):
     return example['image'], example['image_name'] if return_image_name else 0
 
  
-def prepare_image(img, augment=True, dim=256, droprate=0, dropct=0, dropsize=0):    
+def prepare_image(img, cfg=None, augment=True):    
     img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, [cfg['read_size'], cfg['read_size']])
     img = tf.cast(img, tf.float32) / 255.0
     
     if augment:
-        img = transform(img,DIM=dim)
-        if (droprate!=0)&(dropct!=0)&(dropsize!=0): 
-            img = dropout(img, DIM=dim, PROBABILITY=droprate, CT=dropct, SZ=dropsize)
+        img = transform(img, cfg)
+        img = tf.image.random_crop(img, [cfg['crop_size'], cfg['crop_size'], 3])
         img = tf.image.random_flip_left_right(img)
         img = tf.image.random_hue(img, 0.01)
         img = tf.image.random_saturation(img, 0.7, 1.3)
         img = tf.image.random_contrast(img, 0.8, 1.2)
         img = tf.image.random_brightness(img, 0.1)
-                      
-    img = tf.reshape(img, [dim,dim, 3])
-            
+
+    else:
+        img = tf.image.central_crop(img, cfg['crop_size'] / cfg['read_size'])
+                                   
+    img = tf.image.resize(img, [cfg['net_size'], cfg['net_size']])
+    img = tf.reshape(img, [cfg['net_size'], cfg['net_size'], 3])
     return img
 
 def count_data_items(filenames):
@@ -120,9 +203,8 @@ def count_data_items(filenames):
          for filename in filenames]
     return np.sum(n)
 
-def get_dataset(files, augment = False, shuffle = False, repeat = False, 
-                labeled=True, return_image_names=True, batch_size=16, dim=256,
-                droprate=0, dropct=0, dropsize=0):
+def get_dataset(files, cfg, augment = False, shuffle = False, repeat = False, 
+                labeled=True, return_image_names=True):
     
     ds = tf.data.TFRecordDataset(files, num_parallel_reads=AUTO)
     ds = ds.cache()
@@ -131,7 +213,7 @@ def get_dataset(files, augment = False, shuffle = False, repeat = False,
         ds = ds.repeat()
     
     if shuffle: 
-        ds = ds.shuffle(1024*2) #if too large causes OOM in GPU CPU
+        ds = ds.shuffle(1024*8)
         opt = tf.data.Options()
         opt.experimental_deterministic = False
         ds = ds.with_options(opt)
@@ -142,72 +224,41 @@ def get_dataset(files, augment = False, shuffle = False, repeat = False,
         ds = ds.map(lambda example: read_unlabeled_tfrecord(example, return_image_names), 
                     num_parallel_calls=AUTO)      
     
-    ds = ds.map(lambda img, imgname_or_label: (
-                prepare_image(img, augment=augment, dim=dim, 
-                              droprate=droprate, dropct=dropct, dropsize=dropsize), 
-                imgname_or_label), 
+    ds = ds.map(lambda img, imgname_or_label: (prepare_image(img, augment=augment, cfg=cfg), 
+                                               imgname_or_label), 
                 num_parallel_calls=AUTO)
     
-    ds = ds.batch(batch_size * REPLICAS)
+    ds = ds.batch(cfg['batch_size'] * REPLICAS)
     ds = ds.prefetch(AUTO)
     return ds
+"""
+def show_dataset(thumb_size, cols, rows, ds):
+    mosaic = PIL.Image.new(mode='RGB', size=(thumb_size*cols + (cols-1), 
+                                             thumb_size*rows + (rows-1)))
+   
+    for idx, data in enumerate(iter(ds)):
+        img, target_or_imgid = data
+        ix  = idx % cols
+        iy  = idx // cols
+        img = np.clip(img.numpy() * 255, 0, 255).astype(np.uint8)
+        img = PIL.Image.fromarray(img)
+        img = img.resize((thumb_size, thumb_size), resample=PIL.Image.BILINEAR)
+        mosaic.paste(img, (ix*thumb_size + ix, 
+                           iy*thumb_size + iy))
 
-def binary_focal_loss(gamma=2., alpha=.25):
-    """
-    Binary form of focal loss.
-      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
-      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
-    References:
-        https://arxiv.org/pdf/1708.02002.pdf
-    Usage:
-     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
-    """
-    def binary_focal_loss_fixed(y_true, y_pred):
-        """
-        :param y_true: A tensor of the same shape as `y_pred`
-        :param y_pred:  A tensor resulting from a sigmoid
-        :return: Output tensor.
-        """
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-
-        epsilon = K.epsilon()
-        # clip to prevent NaN's and Inf's
-        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
-        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
-
-        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
-               -K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-
-    return binary_focal_loss_fixed
-
+    display(mosaic)
     
-def build_model(dim=128):
-    inp = tf.keras.layers.Input(shape=(dim,dim,3))
-    base = efn.EfficientNetB7(input_shape=(dim,dim,3),weights='imagenet',pooling='avg',include_top=False)
-    x = base(inp)
-    # x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(128,activation='gelu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.Dense(182,activation='gelu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dense(1,activation='sigmoid')(x)
-    model = tf.keras.Model(inputs=inp,outputs=x)
-    opt = tf.keras.optimizers.Adam(learning_rate=0.00001)
-    loss = [binary_focal_loss(gamma=2,alpha=0.12)]
-    model.compile(optimizer=opt,loss=loss,metrics=['AUC'])
-    return model
+    Doesn't work from terminal only in notebooks
 
-"""**Learning rate scheduler**"""
+    """
 
-def get_lr_callback(batch_size=8):
-    lr_start   = 0.0005
-    lr_max     = 0.000125 * REPLICAS * batch_size
-    lr_min     = 0.00001
-    lr_ramp_ep = 5
-    lr_sus_ep  = 0
-    lr_decay   = 0.8
+def get_lr_callback(cfg):
+    lr_start   = cfg['LR_START']
+    lr_max     = cfg['LR_MAX'] * strategy.num_replicas_in_sync
+    lr_min     = cfg['LR_MIN']
+    lr_ramp_ep = cfg['LR_RAMPUP_EPOCHS']
+    lr_sus_ep  = cfg['LR_SUSTAIN_EPOCHS']
+    lr_decay   = cfg['LR_EXP_DECAY']
    
     def lrfn(epoch):
         if epoch < lr_ramp_ep:
@@ -221,44 +272,73 @@ def get_lr_callback(batch_size=8):
             
         return lr
 
-    lr_callback = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=False)
+    lr_callback = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=1)
     return lr_callback
 
-LR = 0.00004;WARMUP = 5
+es=tf.keras.callbacks.EarlyStopping(monitor='val_auc',patience=4,
+                                    mode='max',restore_best_weights=False,verbose=1)
 
-def get_cosine_schedule_with_warmup(lr,num_warmup_steps, num_training_steps, num_cycles=0.5):
-    def lrfn(epoch):
-        if epoch < num_warmup_steps:
-            return (float(epoch) / float(max(1, num_warmup_steps))) * lr
-        progress = float(epoch - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))) * lr
+def get_model(cfg):
+    model_input = tf.keras.Input(shape=(cfg['net_size'], cfg['net_size'], 3), name='imgIn')
 
-    return tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=True)
+    dummy = tf.keras.layers.Lambda(lambda x:x)(model_input)
 
-lr_schedule= get_cosine_schedule_with_warmup(lr=LR,num_warmup_steps=WARMUP,num_training_steps=EPOCHS[0])
+    constructor = getattr(efn, f'EfficientNetB6')
+    x = constructor(include_top=False, weights='imagenet', 
+                    input_shape=(cfg['net_size'], cfg['net_size'], 3), 
+                    pooling='avg')(dummy)
+    x = tf.keras.layers.Dropout(0.3,seed=1024)(x)
+    x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    outputs = [x]
+        
+    model = tf.keras.Model(model_input, outputs, name='aNetwork')
+    model.summary()
+    return model
 
+def compile_new_model(cfg):    
+    with strategy.scope():
+        model = get_model(cfg)
+    
+        losses = [tf.keras.losses.BinaryCrossentropy(label_smoothing = cfg['label_smooth_fac'])]
+        
+        model.compile(
+            optimizer = cfg['optimizer'],
+            loss      = losses,
+            metrics   = [tf.keras.metrics.AUC(name='auc')])
+        
+    return model
 
-"""**Model Training**"""
+nsplits = 5 #no of splits 
+rand = 1024
+folds = KFold(n_splits=nsplits, shuffle = True, random_state = rand)
 
-with strategy.scope():
-        model = build_model(dim=IMG_SIZES[0])
+# pred_tr = pd.DataFrame()
+cnt = 0
+for i,(tr_idx,va_idx) in enumerate(folds.split(files_train)):
+    print("#"*20+"\n split"+str(i+1))
+    CFG['batch_size'] = bs
+    files_train_tr = files_train[tr_idx]
+    files_train_va = files_train[va_idx]
+    ds_train     = get_dataset(files_train_tr, CFG, augment=True, shuffle=True, repeat=True)
 
-model.summary()
+    ds_train     = ds_train.map(lambda img, label: (img, tuple([label])))
+    
+    ds_valid = get_dataset(files_train_va,CFG,augment=False,shuffle=False,repeat=False)
 
-es=tf.keras.callbacks.EarlyStopping(monitor='val_auc',patience=5,restore_best_weights=True,mode='max')
+    ds_valid = ds_valid.map(lambda img,label:(img, tuple([label])))
 
+    steps_train  = count_data_items(files_train_tr) / (CFG['batch_size'] * REPLICAS)
 
-# tf=tf.keras.callbacks.TensorBoard(log_dir='gs://project-285401/logs')
+    steps_valid = count_data_items(files_train_va) / (CFG['batch_size']* REPLICAS)
 
-history = model.fit(
-        get_dataset(files_train, augment=True, shuffle=True, repeat=True,
-                dim=IMG_SIZES[0], batch_size = BATCH_SIZES[0],
-                droprate=DROP_FREQ[1], dropct=DROP_CT[1], dropsize=DROP_SIZE[1]), 
-        epochs=EPOCHS[0], callbacks = [es,lr_schedule], 
-        steps_per_epoch=count_data_items(files_train)/BATCH_SIZES[0]//REPLICAS,
-        validation_data=get_dataset(files_valid,augment=False,shuffle=False,
-                repeat=False,dim=IMG_SIZES[0]),
-        validation_steps=count_data_items(files_valid)/BATCH_SIZES[0]//REPLICAS
-          )
+    model        = compile_new_model(CFG)
 
-# model.save('train.h5')
+    history      = model.fit(ds_train, 
+                         verbose          = 1,
+                         steps_per_epoch  = steps_train, 
+                         epochs           = CFG['epochs'],
+                         validation_data = ds_valid,
+                         validation_steps = steps_valid,
+                         callbacks        = [get_lr_callback(CFG),es])
+
+    model.save('new_train_models'+str(i+1)+'.h5')
